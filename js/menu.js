ecdbb80da2f486f31c8983a0dbdf7d927b39ddb3
DevRyanCosta <devryancosta@gmail.com>
2022-11-07 16:43:43 -0300

    Improved interface. Added left bar. Added responsivity.

--- js/menu.js
+++ js/menu.js
@@ -6,9 +6,11 @@ window.onload = () => {
         topArea.style.display = 'block';
         topArea.style.transform = 'translate(calc(100vw - ' + topArea.offsetWidth + 'px))';
         topArea.style.boxShadow = '0 0 25px 1px #FFF1';
+        topArea.style.opacity = '1';
         closeButton.onclick = () => {
             topArea.style.transform = 'translate(100vw)';
-            topAreaContent = topArea.innerHTML;
+                topArea.style.opacity = '0';
+                topAreaContent = topArea.innerHTML;
             window.setTimeout(() => {
                 topArea.style.display = 'none';
                 topArea.style.transform = 'translate(100vw)';
@@ -17,16 +19,22 @@ window.onload = () => {
     };
     window.onresize = () => {
         setTimeout(() => {
-            topArea.style.display = 'none';
+            if (window.matchMedia("(max-width: 950px)").matches == true) {
+                topArea.style.display = 'none';
+            } else {
+                topArea.style.display = null;
+            }
         }, 350);
         topArea.style.transform = null;
         closeButton.style.display = null;
     }
     window.onscroll = () => {
-        setTimeout(() => {
-            topArea.style.display = 'none';
-        }, 350);
-        topArea.style.transform = null;
-        closeButton.style.display = null;
+        if (window.matchMedia("(max-width: 950px)").matches == true) {
+            setTimeout(() => {
+                topArea.style.display = 'none';
+            }, 350);
+            topArea.style.transform = null;
+            closeButton.style.display = null;
+        }
     }
 };
